0100c95f0958808c2fec2f8cf1621708356a4e49
Anupam Bhakta <anupambhakta1@gmail.com>
2022-09-05 18:18:16 -0400

    first commit

---
+++ price.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    double volatity;
+    double prince_of_asset;
+    double strike_price;
+    double time_exp;
+    double interest_rate;
+    double option_price;
+
+    std::cout << "What would you like to calculate?\n";
+
+    return 0;
+}
